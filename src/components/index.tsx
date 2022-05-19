69171ea8ecf5a061158b267da36fec5df53072fd
CrawlTheDev <hamzaadamis@icloud.com>
2022-05-19 04:14:58 -0700

    add loading text to dynamic imports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,8 +3,18 @@ import dynamic from "next/dynamic";
 export const DynamicHeaderComponent = dynamic(() => import('./Header'), {
      loading: () => <>loading</>
 });
-export const DynamicFooterComponent = dynamic(() => import('./Footer'));
-export const DynamicNavigationBarComponent = dynamic(() => import('./NavigationBar'));
-export const DynamicCompanyBarComponent = dynamic(() => import('./CompanyBar'));
-export const DynamicPageComponent = dynamic(() => import('./Page'));
-export const DynamicHeroComponent = dynamic(() => import('./Hero'));
+export const DynamicFooterComponent = dynamic(() => import('./Footer'), {
+     loading: () => <>loading</>
+});
+export const DynamicNavigationBarComponent = dynamic(() => import('./NavigationBar'), {
+     loading: () => <>loading</>
+});
+export const DynamicCompanyBarComponent = dynamic(() => import('./CompanyBar'), {
+     loading: () => <>loading</>
+});
+export const DynamicPageComponent = dynamic(() => import('./Page'), {
+     loading: () => <>loading</>
+});
+export const DynamicHeroComponent = dynamic(() => import('./Hero'), {
+     loading: () => <>loading</>
+});
