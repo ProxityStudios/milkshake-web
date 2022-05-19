b95a75192ebd1cf0fb692d376903badcd00eb1e0
CrawlTheDev <hamzaadamis@icloud.com>
2022-05-19 04:16:55 -0700

    use Inter font family from google

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,12 +11,12 @@ export default class Document extends NextDocument {
           <link rel="shortcut icon" href="/img/milkshake-logo.png" />
           <link rel="apple-touch-icon" href="/img/milkshake-logo.png" />
           <link rel="manifest" href="/manifest.json" />
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
-          /> */}
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode={config.initialColorMode} />
